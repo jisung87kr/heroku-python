9d4f32c9be71cbf663286655a7a6ea50c0d76f18
jisung <jisung87kr@nate.com>
2019-03-02 01:10:12 +0900

    add file upload function in admin

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,21 @@
 from django.utils import timezone
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     title = models.CharField(max_length=200, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
+    upload = models.FileField(upload_to='%Y/%m/%d/', null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+
+#게시글 삭제시 업로드된 파일도 삭제
+@receiver(post_delete, sender=Post)
+def submission_delete(sender, instance, **kwargs):
+    instance.upload.delete(False) 
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 
@@ -20,3 +22,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
